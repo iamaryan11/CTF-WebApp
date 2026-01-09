4957fb35761fe31d8f6fd4bfea26a98710e521d7
Aryan <aryaneos11@gmail.com>
2026-01-10 01:32:24 +0530

    update redis credss

--- BackEnd/src/config/redis.js
+++ BackEnd/src/config/redis.js
@@ -1,10 +1,10 @@
 const {createClient}=require('redis');
 const redisClient = createClient({
     username: 'default',
-    password: 'TakN4v2gJsBIb56KwmOxxT19PGQFUdvz',
+    password: 'IlW80SG9JtwfhZRjOdm2mygeaAFVVvkG',
     socket: {
-        host: 'redis-18111.crce263.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 18111,
+        host: 'redis-18195.c264.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 18195,
           tls: false, 
     connectTimeout: 10000,
     }
